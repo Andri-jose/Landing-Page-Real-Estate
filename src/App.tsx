34b907be5d63e16ac501501bbaea94b516ac703c
Andri Salazar <andr-y2008@hotmail.com>
2026-01-26 09:45:06 -0300

    first commit without tailwind

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-red-500">
+      <>icono</>
+      <h1>Altavista Residences</h1>
+      <p>Invest Smart, Live Better</p>
+      <>video</>
+      <p>Discover in our <b>exclusive webinar</b> the real state investment opportunity you're been waiting for</p>
+      <button>Register for webinar</button>
+      <>boton flecha</>
+      
+      <div className='form-container'>
+        <div>
+          <h3>Register for the webinar</h3>
+          <p>Complete your details and secure your spot at the exclusive presentation</p>
+        </div>
+        <form>
+          <label htmlFor="full-name">Your Full Name</label>
+          <input type="text" id="full-name" placeholder="Enter your full name" />
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+          <label htmlFor="phone">Your Phone Number</label>
+          <input type="tel" id="phone" placeholder="Enter your phone number" />
+          <button>Confirm Registration</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
