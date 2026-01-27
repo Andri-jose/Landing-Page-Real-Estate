4fa3791acdb2b7d41bd7e7ea5dda7656bb5436ab
Andri Salazar <andr-y2008@hotmail.com>
2026-01-27 17:21:35 -0300

    adding form

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,49 @@
 import altavistaLogo from './assets/icons/altavista-logo.png'
 import interior from './assets/interior-render.jpg'
 import amenities from './assets/amenities.jpg'
-// import './App.css'
+import flecha from './assets/icons/flecha.png'
 
 function App() {
 
   return (
-    <div className="flex flex-col items-center mx-auto text-center">
-      <header className='flex flex-col items-center'>
-        <img src={altavistaLogo} className='w-10'/>
+    <div className="flex justify-between flex-col items-center mx-auto text-center">
+      <header className='flex flex-col items-center justify-between mb-6 mt-5 h-36'>
+        <img src={altavistaLogo} className='w-11'/>
         <h1 className="text-purple text-6xl font-semibold">Altavista Residences</h1>
-        <p>Invest Smart, Live Better</p>
+        <p className="text-subtitle text-2xl font-semibold">Invest Smart, Live Better</p>
       </header>
       <section className='flex flex-col items-center'>   
         <video 
         src="/video/proyecto inmobiliario video.mp4" 
         controls  
-        className='w-96'> 
-        </video>  
-        <p>Discover in our <b>exclusive webinar</b> the real state investment opportunity you're been waiting for</p>
-        <button>Register for webinar</button>
-        <>boton flecha</>
+        className='w-180.75'> 
+        </video>
+        <div className='flex flex-col items-center justify-between h-54 mb-40'>
+          <p className='mt-5 text-lg'>Discover in our <b>exclusive webinar</b> the real state investment opportunity you're been waiting for</p>
+          <button className='bg-button hover:bg-hover-button text-white font-semibold py-4 px-4 rounded-lg cursor-pointer w-60 text-lg'>Register for webinar</button>
+          <button className="cursor-pointer"><img src={flecha} className='w-14'/></button>
+        </div>  
       </section>
-      <section className=''> 
+      <section className='mb-40 border-solid border-2 border-purple rounded-lg p-8 w-xl'> 
         <div>
-          <h3>Register for the webinar</h3>
-          <p>Complete your details and secure your spot at the exclusive presentation</p>
+          <h3 className='text-3xl'>Register for the webinar</h3>
+          <p className='text-sm mb-6 mt-2'>Complete your details and secure your spot at the exclusive presentation</p>
         </div>
-        <form>
-          <label htmlFor="full-name">Your Full Name</label>
-          <input type="text" id="full-name" placeholder="Enter your full name" />
-          <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" placeholder="Enter your email" />
-          <label htmlFor="phone">Your Phone Number</label>
-          <input type="tel" id="phone" placeholder="Enter your phone number" />
-          <button>Confirm Registration</button>
+        <form className='flex flex-col items-start justify-evenly h-95 gap-2'>
+          <label htmlFor="full-name">Full Name</label>
+          <input type="text" id="full-name" placeholder="John Smith" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="John@example.com" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <label htmlFor="phone">Phone</label>
+          <input type="tel" id="phone" placeholder="+1(555)123-4567" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <button className='bg-button hover:bg-hover-button text-white font-semibold py-2 px-4 rounded-lg cursor-pointer w-full text-lg items-center mx-auto mt-2'>Confirm Registration</button>
+          <p className='text-xs mt-4'>By registering, you agree to be contacted via whatsapp and email with project information.</p>
         </form>
       </section>
       <section>
         <h2 className='text-5xl'>About Altavista Residences</h2>
         <p>A project designed for those who want to invest in their future without compromising their lifestyle</p>
-        <div className='grid grid-cols-2 gap-2'>
+        <div className='grid grid-cols-2 gap-5 w-6xl'>
           <img src={interior} className='w-full'/>
           <img src={amenities} className='w-full'/>
         </div>
