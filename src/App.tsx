fe6e758dd04889325cbac18d324b6bc69953f5cb
Andri Salazar <andr-y2008@hotmail.com>
2026-01-27 18:44:21 -0300

    adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import altavistaLogo from './assets/icons/altavista-logo.png'
 import interior from './assets/interior-render.jpg'
 import amenities from './assets/amenities.jpg'
-import flecha from './assets/icons/flecha.png'
+import arrow from './assets/icons/icon-button.png'
 
 function App() {
 
@@ -19,33 +19,33 @@ function App() {
         className='w-180.75'> 
         </video>
         <div className='flex flex-col items-center justify-between h-54 mb-40'>
-          <p className='mt-5 text-lg'>Discover in our <b>exclusive webinar</b> the real state investment opportunity you're been waiting for</p>
+          <p className='mt-5 text-lg'>Discover in our <b className='text-highlight'>exclusive webinar</b> the real state investment opportunity you're been waiting for</p>
           <button className='bg-button hover:bg-hover-button text-white font-semibold py-4 px-4 rounded-lg cursor-pointer w-60 text-lg'>Register for webinar</button>
-          <button className="cursor-pointer"><img src={flecha} className='w-14'/></button>
+          <button className="cursor-pointer"><img src={arrow} className='w-14'/></button>
         </div>  
       </section>
-      <section className='mb-40 border-solid border-2 border-purple rounded-lg p-8 w-xl'> 
+      <section className='mb-40 border-solid border border-purple/30 rounded-lg p-8 w-xl'> 
         <div>
-          <h3 className='text-3xl'>Register for the webinar</h3>
+          <h3 className='text-3xl font-semibold'>Register for the webinar</h3>
           <p className='text-sm mb-6 mt-2'>Complete your details and secure your spot at the exclusive presentation</p>
         </div>
         <form className='flex flex-col items-start justify-evenly h-95 gap-2'>
           <label htmlFor="full-name">Full Name</label>
-          <input type="text" id="full-name" placeholder="John Smith" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <input type="text" id="full-name" placeholder="John Smith" className='border-solid border-2 border-white/30 rounded-lg p-2 w-full mb-3' />
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="John@example.com" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <input type="email" id="email" placeholder="John@example.com" className='border-solid border-2 border-white/30 rounded-lg p-2 w-full mb-3' />
           <label htmlFor="phone">Phone</label>
-          <input type="tel" id="phone" placeholder="+1(555)123-4567" className='border-solid border-2 border-purple rounded-lg p-2 w-full mb-3' />
+          <input type="tel" id="phone" placeholder="+1(555)123-4567" className='border-solid border-2 border-white/30 rounded-lg p-2 w-full mb-3' />
           <button className='bg-button hover:bg-hover-button text-white font-semibold py-2 px-4 rounded-lg cursor-pointer w-full text-lg items-center mx-auto mt-2'>Confirm Registration</button>
           <p className='text-xs mt-4'>By registering, you agree to be contacted via whatsapp and email with project information.</p>
         </form>
       </section>
-      <section>
-        <h2 className='text-5xl'>About Altavista Residences</h2>
+      <section className='flex flex-col items-center justify-between h-139'>
+        <h2 className='text-5xl'>About <b className='text-purple'>Altavista Residences</b></h2>
         <p>A project designed for those who want to invest in their future without compromising their lifestyle</p>
         <div className='grid grid-cols-2 gap-5 w-6xl'>
-          <img src={interior} className='w-full'/>
-          <img src={amenities} className='w-full'/>
+          <img src={interior} className='w-full rounded-lg'/>
+          <img src={amenities} className='w-full rounded-lg'/>
         </div>
       </section>
       
